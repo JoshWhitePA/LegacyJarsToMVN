83aa8ecc91c4fdf184d88cb8f4cd03e748cf625c
Joshua White <josh@Joshuas-MacBook-Pro.local>
2018-02-16 17:38:30 -0500

    CHecking maven

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 console.log("Here");
 $(document).ready(function () {
     console.log("ready!");
+    searchMVN("2d3c16092663da9041b171b8d3627cbafa8f0cb1");
 });
 
 
@@ -41,6 +42,33 @@ var readThatFile = function (myFile) {
         }
     }
 };
+
+
+var processJSON = function(jsonData){
+    var jarInfo = jsonData.response.docs;
+    console.log("jar info: ");
+    console.log(jarInfo.g);
+    
+};
+var searchMVN = function(hashValue){
+   var url = "http://search.maven.org/solrsearch/select?q=1:%22"+hashValue+"%22&rows=20&wt=json";
+   console.log(url);
+   $.ajax({
+    dataType: "json",
+    url: "http://search.maven.org/solrsearch/select",
+    data: {
+        q:"1:%22"+hashValue+"%22",
+        rows:"20",
+        wt:"json"
+    },
+    success: function(data, status){
+        console.log(data);
+        processJSON(data);
+    }
+  });
+
+};
+
 $("#muhJars").change(pullfiles);
 
 
